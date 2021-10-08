49428200dac3b006cad2d57e68867f027c47ed44
Seweryn Woźniak <sewerynwozniak111$gmail.com>
2021-10-08 23:58:42 +0200

    fixing class constructor

--- script.js
+++ script.js
@@ -1,39 +1,32 @@
-const connectBoard = document.querySelector('.connect-board')    
-const dots = document.querySelectorAll('.dot');
-const button = document.querySelector('button')
-
-
-
-let connArr=[
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null],
-]
-
-
-
-
-
 class Connect4{
 
-    connectBoard = document.querySelector('.connect-board');
-    currentPlayer;
-    listenerReff = (e)=> this.clickColumn(e);
-    
+     
     constructor(){
-        this.connectBoard.addEventListener('click', this.listenerReff);
+        this.connectBoard = document.querySelector('.connect-board');
+        this.button = document.querySelector('button')
         this.currentPlayer=1;
+        this.gameIsGoing = true;
+        this.connArr=[
+            [null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null],
+        ]
+        this.actions();
     }
 
 
+    actions(){
+        this.connectBoard.addEventListener('click', (e)=>this.clickColumn(e));
+    }
+
 
     clickColumn(e){
-        let row = e.target.getAttribute('data-row');
+     
         let column = e.target.getAttribute('data-column');
 
         this.checkIfFree(column);
@@ -54,10 +47,10 @@ class Connect4{
 
         //if(connArr[5][column] != null) return;
 
-        for(let i=0;i<connArr.length;i++){
+        for(let i=0;i<this.connArr.length;i++){
 
-            if(connArr[i][column] == null){
-                connArr[i][column] = this.currentPlayer;
+            if(this.connArr[i][column] == null){
+                this.connArr[i][column] = this.currentPlayer;
                 this.takeSlotAnimation(i, column);
                 return;
             }
@@ -67,27 +60,30 @@ class Connect4{
     }
 
 
+
     horizontalCheckWin(i ,j,player){
-        if(connArr[i][j]==player && connArr[i][j+1]==player && connArr[i][j+2]==player && connArr[i][j+3]==player){
-            console.log('zwyciestwo', player);
+        if(this.connArr[i][j]==player && this.connArr[i][j+1]==player && this.connArr[i][j+2]==player && this.connArr[i][j+3]==player){
+            console.log('zwyciestwo', player);      
            return this.finishGame();
         }
+        
     }
     skewRightCheckWin(i ,j,player){
-        if(connArr[i][j]==player && connArr[i+1][j+1]==player && connArr[i+2][j+2]==player && connArr[i+3][j+3]==player){
+        if(this.connArr[i][j]==player && this.connArr[i+1][j+1]==player && this.connArr[i+2][j+2]==player && this.connArr[i+3][j+3]==player){
             console.log('zwyciestwo', player);
             return this.finishGame();
         }
     }
     skewLeftCheckWin(i ,j,player){
-        if(connArr[i][j]==player && connArr[i+1][j-1]==player && connArr[i+2][j-2]==player && connArr[i+3][j-3]==player){
+        if(this.connArr[i][j]==player && this.connArr[i+1][j-1]==player && this.connArr[i+2][j-2]==player && this.connArr[i+3][j-3]==player){
             console.log('zwyciestwo', player);
             return this.finishGame();
         }
     }
     verticalCheckWin(i ,j,player){
-        if(connArr[i][j]==player && connArr[i+1][j]==player && connArr[i+2][j]==player && connArr[i+3][j]==player){
+        if(this.connArr[i][j]==player && this.connArr[i+1][j]==player && this.connArr[i+2][j]==player && this.connArr[i+3][j]==player){
             console.log('zwyciestwo', player);
+            
             return this.finishGame();
         }
     }
@@ -95,30 +91,35 @@ class Connect4{
 
     checkIfWin(){
 
-        for(let i=0;i<connArr.length;i++){
+        
+        for(let i=0;i<this.connArr.length;i++){
 
-            //horizontal
-            for(let j=0;j<connArr[i].length-3;j++){
-                this.horizontalCheckWin(i,j,1)
-                this.horizontalCheckWin(i,j,2)               
-            }
-            //skew right
-            for(let j=0;j<connArr[i].length-3;j++){
-                this.skewRightCheckWin(i,j,1)
-                this.skewRightCheckWin(i,j,2)          
-            }
-            //skew left
-            for(let j=connArr[i].length-1;j>connArr[i].length-5;j--){
-                this.skewLeftCheckWin(i,j,1)
-                this.skewLeftCheckWin(i,j,2)
-            }
-            //vertical
-            if(i<=2){
-            for(let j=0;j<=connArr[i].length-1;j++){
-                this.verticalCheckWin(i,j,1)
-                this.verticalCheckWin(i,j,2)
-            }
-            }
+            if(!this.gameIsGoing){break;}
+          
+                //horizontal
+                for(let j=0;j<this.connArr[i].length-3;j++){
+                    this.horizontalCheckWin(i,j,1)
+                    this.horizontalCheckWin(i,j,2)               
+                }
+                //skew right
+                for(let j=0;j<this.connArr[i].length-3;j++){
+                    this.skewRightCheckWin(i,j,1)
+                    this.skewRightCheckWin(i,j,2)          
+                }
+                //skew left
+                for(let j=this.connArr[i].length-1;j>this.connArr[i].length-5;j--){
+                    this.skewLeftCheckWin(i,j,1)
+                    this.skewLeftCheckWin(i,j,2)
+                }
+                //vertical
+                if(i<=2){
+                for(let j=0;j<=this.connArr[i].length-1;j++){
+                    this.verticalCheckWin(i,j,1)
+                    this.verticalCheckWin(i,j,2)
+                }
+                }
+        
+
 
             }
         
@@ -126,7 +127,7 @@ class Connect4{
 
 
 
-    
+
     takeSlotAnimation(row, column){
 
         let slot = document.querySelector(`[data-row='${row}'][data-column='${column}']`)
@@ -140,13 +141,9 @@ class Connect4{
 
 
     finishGame(){
-
+        this.gameIsGoing = false;
         this.connectBoard.removeEventListener('click', this.listenerReff);
-        connArr = connArr.forEach(el=>el.map(e=>null));
-
-        
-
-        console.log(connArr)
+        //connArr = this.connArr.forEach(el=>el.map(e=>null));
 
 
     }
